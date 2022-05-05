a108f58cfedef00073688fc72c506dd3f091c293
rachel-meadows <rachelnewzealand@gmail.com>
2022-05-05 12:34:56 +1200

    Add 'quotes' db functions

---
+++ server/db/quotes.js
@@ -0,0 +1,48 @@
+const connection = require('./connection')
+
+function addQuote(input, db = connection) {
+  const { userId, jobId, price, dateAdded, notes, status } = input
+  const quote = {
+    user_id: userId,
+    job_id: jobId,
+    price,
+    date_added: dateAdded,
+    notes,
+    status,
+  }
+  return db('quotes').insert(quote)
+}
+
+function getQuotesByCustomer(id, db = connection) {
+  return db('quotes')
+    .where('user_id', id)
+    .select(
+      'id',
+      'user_id as userId',
+      'job_id as jobId',
+      'price',
+      'date_added as dateAdded',
+      'notes',
+      'status'
+    )
+}
+
+function getQuotesByJob(id, db = connection) {
+  return db('quotes')
+    .where('job_id', id)
+    .select(
+      'id',
+      'user_id as userId',
+      'job_id as jobId',
+      'price',
+      'date_added as dateAdded',
+      'notes',
+      'status'
+    )
+}
+
+module.exports = {
+  addQuote,
+  getQuotesByCustomer,
+  getQuotesByJob,
+}
