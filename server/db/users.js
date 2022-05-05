1d27849dedf166ef5a9f1660784e9b93d5a10dfb
rachel-meadows <rachelnewzealand@gmail.com>
2022-05-05 12:11:12 +1200

    Add 'users' db functions

--- server/db/users.js
+++ server/db/users.js
@@ -4,15 +4,15 @@ function getUsers(db = connection) {
   return db('users').select(
     'id',
     'auth0_id as auth0Id',
-    'name',
     'email',
-    'description'
+    'type',
+    'business_id as businessId'
   )
 }
 
 function addUser(input, db = connection) {
-  const { auth0Id, name, email, description } = input
-  const user = { auth0_id: auth0Id, name, email, description }
+  const { auth0Id, email, type, businessId } = input
+  const user = { auth0_id: auth0Id, email, type, business_id: businessId }
   return db('users').insert(user)
 }
 
