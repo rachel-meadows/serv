b6c362c640c983453db306edc631ff12cbf26057
Genevieve-Sage <gensage11@gmail.com>
2022-05-05 17:10:37 +1200

    started quotes components for customers

---
+++ client/components/Customer/QuotesList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+//import QuotesItem from './QuotesItem'
+
+function QuotesList() {
+  return (
+    <div>
+      <h1>Customer Quotes</h1>
+      <form>
+        <input type="text" />
+      </form>
+    </div>
+  )
+}
+
+export default QuotesList
