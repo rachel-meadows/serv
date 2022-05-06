e384efd11cc6cbff739d01b620d2b9d5154424ea
Daniel Bird <danieldbird@gmail.com>
2022-05-06 21:59:25 +1200

    style basic home page

--- client/components/home/Home.jsx
+++ client/components/home/Home.jsx
@@ -1,16 +1,24 @@
+import { useAuth0 } from '@auth0/auth0-react'
 import React from 'react'
+import { getRegisterFn } from '../../auth0-utils'
 
 function Home() {
+  const register = getRegisterFn(useAuth0)
+  function handleRegister(event) {
+    event.preventDefault()
+    register()
+  }
   return (
-    <div>
-      <h1>Serv.</h1>
-      {/* <p>
-        Need a plumber? A gardener? A roofer? Your search is over. Just describe
-        your job, then get quotes from local businesses.
-      </p> */}
-      <div className="info-buttons">
-        <button className="how-it-works">How it Works</button>
-        <button className="services">Services</button>
+    <div className="home-page">
+      <div className="hero">
+        <img src="/serv-logo-dark.png" alt="Serv." />
+        <p className="tagline">
+          Need a plumber? A gardener? A roofer? Your search is over. Just
+          describe your job, then get quotes from local businesses.
+        </p>
+        <button className="sign-up-btn" onClick={handleRegister}>
+          Sign Up!
+        </button>
       </div>
     </div>
   )
