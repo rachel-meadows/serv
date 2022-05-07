a0c9044e0fdf4c34d4466db4384fa6201af417b0
rachel-meadows <rachelnewzealand@gmail.com>
2022-05-07 21:25:39 +1200

    Display unmatched job listings from state

--- client/components/business/BusinessJobsList.jsx
+++ client/components/business/BusinessJobsList.jsx
@@ -3,41 +3,31 @@ import { useDispatch, useSelector } from 'react-redux'
 import BusinessJobItem from './BusinessJobItem'
 
 import { useNavigate } from 'react-router-dom'
-import { APIgetBusinessByUserId } from '../../apis/business'
+// import { APIgetBusinessByUserId } from '../../apis/business'
 // import { fetchJobsByUserId } from '../../actions/business'
+
 import {
-  fetchOpenJobs,
   fetchOpenJobsByCategory,
   fetchJobsByUser,
 } from '../../actions/business'
 
 function BusinessJobsList({ children }) {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
-  const allJobs = useSelector((state) => state.jobList)
   const currentUser = useSelector((state) => state.currentUser)
   const openJobs = useSelector((state) => state.openJobsByCategory)
   const jobsByUser = useSelector((state) => state.jobsByUser)
-  const jobListing = useSelector((state) => state.openJobs)
   // const { userId, category } = userData
   // const business = APIgetBusinessByUserId(currentUser.id)
 
   const [jobs, setJobs] = useState(openJobs)
-  const [dropDownSelection, setdropDownSelection] = useState('all')
-
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-
+  const [dropDownSelection, setdropDownSelection] = useState('unmatched')
 
   useEffect(() => {
-    console.log("1")
     dispatch(fetchOpenJobsByCategory('construction'))
-
-  }, [])
-
-  useEffect(() => {
     dispatch(fetchJobsByUser(3))
   }, [])
-
 
   // useEffect(() => {
   //   // dispatch(fetchJobsByCatergory(userId, category))
@@ -45,39 +35,26 @@ function BusinessJobsList({ children }) {
   //   dispatch(fetchJobsByUser(currentUser.id))
   // }, [])
 
-
-
   useEffect(() => {
     if (dropDownSelection === 'unmatched') {
-      console.log("4")
       setJobs(openJobs)
-      console.log(jobs);
-    }
-    else if (dropDownSelection === 'quoted') {
+    } else if (dropDownSelection === 'quoted') {
       setJobs(jobsByUser.filter((obj) => obj.status === 'quoted'))
-    }
-    else if (dropDownSelection === 'active') {
+    } else if (dropDownSelection === 'active') {
       setJobs(jobsByUser.filter((obj) => obj.status === 'in progress'))
-    }
-    else if (dropDownSelection === 'completed') {
+    } else if (dropDownSelection === 'completed') {
       setJobs(jobsByUser.filter((obj) => obj.status === 'closed'))
     }
-    // else if (dropDownSelection === 'all') {
-    //   setJobs(allJobs)
-    // }
   }, [jobsByUser, openJobs, dropDownSelection])
 
   function showDetails(jobsId, status) {
     if (status === 'open') {
       navigate(`/business/open/${jobsId}`)
-    }
-    else if (status === 'quoted') {
+    } else if (status === 'quoted') {
       navigate(`/business/quoted/${jobsId}`)
-    }
-    else if (status === 'in progress') {
+    } else if (status === 'in progress') {
       navigate(`/business/active/${jobsId}`)
-    }
-    else if (status === 'closed') {
+    } else if (status === 'closed') {
       navigate(`/business/completed/${jobsId}`)
     }
   }
@@ -86,12 +63,15 @@ function BusinessJobsList({ children }) {
     setdropDownSelection(event.target.value)
   }
 
-
   return (
     <>
       <form>
         <label htmlFor="filter">Filter your jobs:</label>
-        <select name="filter" defaultValue="unmatched" onChange={handleDropDown} >
+        <select
+          name="filter"
+          defaultValue="unmatched"
+          onChange={handleDropDown}
+        >
           {/* <option value="all">All</option> */}
           <option value="unmatched">Unmatched</option>
           <option value="quoted">Quoted</option>
@@ -101,19 +81,21 @@ function BusinessJobsList({ children }) {
       </form>
       <h1>Job Listings</h1>
       <div className="jobList">
+        {jobs?.map((jobListing) => {
+          return (
+            <BusinessJobItem
+              key={jobListing.id}
+              jobListing={jobListing}
+              showDetails={showDetails}
+            />
+          )
 
-        {
-          jobs?.map((jobListing) => {
-            console.log(jobListing)
-            return <BusinessJobItem key={jobListing.id} jobListing={jobListing} showDetails={showDetails} />
-
-            //   {children} {/* This holds the WaitIndicator (from App) */}
-            //   {jobListings?.map((jobListing) => {
-            // return jobListing.description       Rachael to review this page/line
-            // return <BusJobItem key={jobListing.id} jobListing={jobListing} />
-          })
-        }
-      </div >
+          //   {children} {/* This holds the WaitIndicator (from App) */}
+          //   {jobListings?.map((jobListing) => {
+          // return jobListing.description       Rachael to review this page/line
+          // return <BusJobItem key={jobListing.id} jobListing={jobListing} />
+        })}
+      </div>
     </>
   )
 }
