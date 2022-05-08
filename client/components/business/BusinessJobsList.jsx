b167cf0fb72a4fb5b629178e15ad378a58c3f8b8
Daniel Bird <danieldbird@gmail.com>
2022-05-08 21:40:30 +1200

    add timeout for message

--- client/components/business/BusinessJobsList.jsx
+++ client/components/business/BusinessJobsList.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import BusinessJobItem from './BusinessJobItem'
 
 import { useLocation, useNavigate } from 'react-router-dom'
-// import { APIgetBusinessByUserId } from '../../apis/business'
+import { APIgetBusinessByUserId } from '../../apis/business'
 
 import {
   // fetchOpenJobs,
@@ -12,7 +12,6 @@ import {
 } from '../../actions/business'
 
 function BusinessJobsList({ children }) {
-
   const allJobs = useSelector((state) => state.jobList)
   const currentBusiness = useSelector((state) => state.currentBusiness)
   const currentUser = useSelector((state) => state.currentUser)
@@ -27,21 +26,20 @@ function BusinessJobsList({ children }) {
   const [business, setBusiness] = useState({})
   const [jobs, setJobs] = useState(openJobs)
   const [dropDownSelection, setdropDownSelection] = useState('unmatched')
-
+  const [showMessage, setShowMessage] = useState(false)
   // const navigate = useNavigate()
   const dispatch = useDispatch()
   const userId = currentUser.id
- 
+
   useEffect(() => {
     console.log('1')
     dispatch(fetchOpenJobsByCategory('plumbing'))
   }, [])
 
   useEffect(() => {
-    APIgetBusinessByUserId(currentUser.id)
-      .then((data) => {
-        setBusiness(data)
-      }).catch
+    APIgetBusinessByUserId(currentUser.id).then((data) => {
+      setBusiness(data)
+    }).catch
   }, [currentUser])
 
   useEffect(() => {
@@ -53,20 +51,34 @@ function BusinessJobsList({ children }) {
   }, [])
 
   useEffect(() => {
+    setShowMessage(location?.state?.message)
+    setTimeout(() => {
+      setShowMessage(false)
+    }, 5000)
+  }, [])
+
+  useEffect(() => {
     if (dropDownSelection === 'unmatched') {
       setJobs(openJobs)
       console.log()
-
-    }
-    else if (dropDownSelection === 'quoted') {
+    } else if (dropDownSelection === 'quoted') {
       // 'pending'status includes pending and rejected quotes
-      setJobs(jobsByUser.filter((obj) => obj.quoteStatus === 'pending' || 'rejected'))
-    }
-    else if (dropDownSelection === 'active') {
-      setJobs(jobsByUser.filter((obj) => obj.jobStatus === 'in progress' && obj.quoteStatus === 'accepted'))
-    }
-    else if (dropDownSelection === 'completed') {
-      setJobs(jobsByUser.filter((obj) => obj.jobStatus === 'closed' && obj.quoteStatus === 'accepted'))
+      setJobs(
+        jobsByUser.filter((obj) => obj.quoteStatus === 'pending' || 'rejected')
+      )
+    } else if (dropDownSelection === 'active') {
+      setJobs(
+        jobsByUser.filter(
+          (obj) =>
+            obj.jobStatus === 'in progress' && obj.quoteStatus === 'accepted'
+        )
+      )
+    } else if (dropDownSelection === 'completed') {
+      setJobs(
+        jobsByUser.filter(
+          (obj) => obj.jobStatus === 'closed' && obj.quoteStatus === 'accepted'
+        )
+      )
       console.log(jobs)
     }
     // else if (dropDownSelection === 'all') {
@@ -80,7 +92,7 @@ function BusinessJobsList({ children }) {
 
   return (
     <>
-      {location?.state?.message && (
+      {showMessage && (
         <div className="quote-submitted flex flex-col flex-align-center">
           <h2>Your quote has been submitted</h2>
           <p>Your quote has been successfully passed on to your customer.</p>
