fedd9d8e2546870d4c43f62bb72f9df769e5aed6
Daniel Bird <danieldbird@gmail.com>
2022-05-08 21:16:56 +1200

    add a conditional message to top of jobs list when a job is quoted

--- client/components/business/BusinessJobToQuote.jsx
+++ client/components/business/BusinessJobToQuote.jsx
@@ -3,11 +3,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import { createQuote, fetchJobById } from '../../actions/business'
 import { useNavigate, useParams } from 'react-router-dom'
 
-
 function BusinessJobToQuote() {
   const currentJob = useSelector((state) => state.currentJob)
-  console.log(currentJob)
-  const [job, setJob] = useState(currentJob)
   const dispatch = useDispatch()
   const navigate = useNavigate()
   const { jobId } = useParams()
@@ -15,17 +12,12 @@ function BusinessJobToQuote() {
     dispatch(fetchJobById(jobId))
   }, [])
 
-    //need to send businessId to database with quote 
-    //APIgetBusinessByUserId
-
   const [toggleForm, setToggleForm] = useState(false)
   const [quoteForm, setQuoteForm] = useState({
-    description: "",
+    description: '',
     priceMin: 0,
-    priceMax: 0
+    priceMax: 0,
   })
-
-
 
   const handleChange = (event) => {
     const name = event.target.name
@@ -42,32 +34,30 @@ function BusinessJobToQuote() {
     console.log(currentJob.id)
     console.log(quoteForm)
     dispatch(createQuote(currentJob.id, quoteForm))
-    navigate(`/business`)
+    navigate(`/business`, { state: { message: true } })
   }
   return (
-
-
     <>
       <div className="flex flex-col flex-justify-center">
         <div className="jobList-item"></div>
-        <p className="userId" key={currentJob.id}>{currentJob.userId}</p>
+        <p className="userId" key={currentJob.id}>
+          {currentJob.userId}
+        </p>
         <p className="category">{currentJob.category}</p>
         <p className="description">{currentJob.description}</p>
-        <p className="price">Budget: ${currentJob.priceMin} - {currentJob.priceMax}</p>
+        <p className="price">
+          Budget: ${currentJob.priceMin} - {currentJob.priceMax}
+        </p>
         {/* <p className="image">{image}</p> */}
         <p className="status">{currentJob.status}</p>
-
 
         {createQuote && (
           <button className="accept-btn" onClick={handleSetToggleForm}>
             Create Quote
           </button>
         )}
-
       </div>
-
       {toggleForm && (
-
         <>
           <div className="flex flex-col flex-align-center">
             <h2>Send A Message To A Customers</h2>
@@ -86,26 +76,26 @@ function BusinessJobToQuote() {
               placeholder="Min price"
               value={quoteForm.priceMin}
               onChange={handleChange}
-              disabled={false} />
+              disabled={false}
+            />
             <label htmlFor="price_min"></label>
             <input
               name="priceMax"
               placeholder="Max price"
               value={quoteForm.priceMax}
               onChange={handleChange}
-              disabled={false} />
+              disabled={false}
+            />
           </div>
           <div className="flex flex-col flex-justify-center">
-            <button className="submit-button" onClick={handleSubmit}>Submit</button>
+            <button className="submit-button" onClick={handleSubmit}>
+              Submit
+            </button>
           </div>
         </>
       )}
     </>
   )
-
-
 }
-
-
 
 export default BusinessJobToQuote
--- client/components/business/BusinessJobsList.jsx
+++ client/components/business/BusinessJobsList.jsx
@@ -2,41 +2,39 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import BusinessJobItem from './BusinessJobItem'
 
-import { useNavigate } from 'react-router-dom'
-import { APIgetBusinessByUserId } from '../../apis/business'
+import { useLocation, useNavigate } from 'react-router-dom'
+// import { APIgetBusinessByUserId } from '../../apis/business'
 
 import {
-  fetchOpenJobs,
+  // fetchOpenJobs,
   fetchOpenJobsByCategory,
   fetchJobsByUser,
 } from '../../actions/business'
 
 function BusinessJobsList({ children }) {
-
-  const allJobs = useSelector((state) => state.jobList)
+  // const allJobs = useSelector((state) => state.jobList)
   const currentUser = useSelector((state) => state.currentUser)
   const openJobs = useSelector((state) => state.openJobsByCategory)
   const jobsByUser = useSelector((state) => state.jobsByUser)
-  const jobListing = useSelector((state) => state.openJobs)
+  // const jobListing = useSelector((state) => state.openJobs)
+  const location = useLocation()
   // const { userId, category } = userData
   // const business = APIgetBusinessByUserId(currentUser.id)
   console.log(jobsByUser)
   const [jobs, setJobs] = useState(openJobs)
   const [dropDownSelection, setdropDownSelection] = useState('unmatched')
 
-  const navigate = useNavigate()
+  // const navigate = useNavigate()
   const dispatch = useDispatch()
   const userId = currentUser.id
   useEffect(() => {
-    console.log("1")
+    console.log('1')
     dispatch(fetchOpenJobsByCategory('plumbing'))
-
   }, [])
 
   useEffect(() => {
     dispatch(fetchJobsByUser(userId))
   }, [])
-
 
   // useEffect(() => {
   //   // dispatch(fetchJobsByCatergory(userId, category))
@@ -44,21 +42,17 @@ function BusinessJobsList({ children }) {
   //   dispatch(fetchJobsByUser(currentUser.id))
   // }, [])
 
-
   useEffect(() => {
     if (dropDownSelection === 'unmatched') {
       setJobs(openJobs)
       console.log()
-    }
-    else if (dropDownSelection === 'quoted') {
+    } else if (dropDownSelection === 'quoted') {
       setJobs(jobsByUser.filter((obj) => obj.quoteStatus === 'pending'))
       console.log(jobs)
-    }
-    else if (dropDownSelection === 'active') {
+    } else if (dropDownSelection === 'active') {
       setJobs(jobsByUser.filter((obj) => obj.jobStatus === 'in progress'))
       console.log(jobs)
-    }
-    else if (dropDownSelection === 'completed') {
+    } else if (dropDownSelection === 'completed') {
       setJobs(jobsByUser.filter((obj) => obj.jobStatus === 'closed'))
       console.log(jobs)
     }
@@ -67,18 +61,26 @@ function BusinessJobsList({ children }) {
     // }
   }, [jobsByUser, openJobs, dropDownSelection])
 
-
-
   function handleDropDown(event) {
     setdropDownSelection(event.target.value)
   }
 
-
   return (
     <>
+      {location?.state?.message && (
+        <div className="quote-submitted flex flex-col flex-align-center">
+          <h2>Your quote has been submitted</h2>
+          <p>Your quote has been successfully passed on to your customer.</p>
+          <p>Youll get a message soon if they accept the quote.</p>
+        </div>
+      )}
       <form>
         <label htmlFor="filter">Filter your jobs:</label>
-        <select name="filter" defaultValue="unmatched" onChange={handleDropDown} >
+        <select
+          name="filter"
+          defaultValue="unmatched"
+          onChange={handleDropDown}
+        >
           {/* <option value="all">All</option> */}
           <option value="unmatched">Unmatched</option>
           <option value="quoted">Quoted</option>
@@ -88,18 +90,21 @@ function BusinessJobsList({ children }) {
       </form>
       <h1>Job Listings</h1>
       <div className="jobList">
+        {jobs?.map((job) => {
+          return (
+            <BusinessJobItem
+              key={job.id}
+              job={job}
+              dropDownSelection={dropDownSelection}
+            />
+          )
 
-        {
-          jobs?.map((job) => {
-            return <BusinessJobItem key={job.id} job={job} dropDownSelection={dropDownSelection} />
-
-            //   {children} {/* This holds the WaitIndicator (from App) */}
-            //   {jobListings?.map((jobListing) => {
-            // return jobListing.description       Rachel to review this page/line
-            // return <BusJobItem key={jobListing.id} jobListing={jobListing} />
-          })
-        }
-      </div >
+          //   {children} {/* This holds the WaitIndicator (from App) */}
+          //   {jobListings?.map((jobListing) => {
+          // return jobListing.description       Rachel to review this page/line
+          // return <BusJobItem key={jobListing.id} jobListing={jobListing} />
+        })}
+      </div>
     </>
   )
 }
