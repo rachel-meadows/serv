b5df0a437218f2003fee3d1ef3b14d2cf07454a8
Katie-Rey <katiejorey@gmail.com>
2022-05-11 09:38:13 +1200

    saving file

--- client/components/business/BusinessJobItem.jsx
+++ client/components/business/BusinessJobItem.jsx
@@ -78,3 +78,24 @@ function BusinessJobItem(props) {
 }
 
 export default BusinessJobItem
+
+
+
+<div className="container mt-3">
+<h2 className="text-success mb-3">Quote</h2>
+<div className="card my-2 p-4 col-xl-6">
+  <table className="table">
+    <tbody>
+    <tr>
+        <th scope="row">Description</th>
+        <td className="text-capitalize">{props.description}</td>
+      </tr>
+      <tr>
+        <th scope="row">Price</th>
+        <td className="text-capitalize">{props.price}</td>
+      </tr>
+    </tbody>
+  </table>
+
+</div>
+</div>
